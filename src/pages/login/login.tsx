75a411236d72c0b472e5ea8c2eb529967d184ad9
ayo-sona <ayo-sona@users.noreply.github.com>
2025-10-29 11:37:24 +0100

    fix: deployment issue

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import "./Login.scss";
+import "./login.scss";
 
 const Login: React.FC = () => {
   const [email, setEmail] = useState("");
